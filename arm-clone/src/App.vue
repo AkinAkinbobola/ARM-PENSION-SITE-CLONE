3a20ac1c78ae5614378b85f127814843dd66ee22
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-15 16:03:56 +0100

    Calculator content

--- arm-clone/src/App.vue
+++ arm-clone/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import Header from './components/Header.vue';
-import Slider from './components/Slider.vue';
-import CardSection from './components/CardSection.vue';
+import Header from './components/Header.vue'
+import Slider from './components/Slider.vue'
+import CardSection from './components/CardSection.vue'
 import NoSolicitors from './components/NoSolicitors.vue'
 import GridSection from './components/GridSection.vue'
+import BenefitCalculator from './components/BenefitCalculator.vue'
 
 </script>
 
@@ -14,5 +15,6 @@ import GridSection from './components/GridSection.vue'
     <CardSection />
     <NoSolicitors />
     <GridSection />
+    <BenefitCalculator />
   </div>
 </template>
--- arm-clone/src/components/BenefitCalculator.vue
+++ arm-clone/src/components/BenefitCalculator.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
-        
+        <div flex flex-col>
+            <h1>Plan your retirement with us</h1>
+            <h1>Can I buy my dream house at retirement?</h1>
+            <p>When you have accumulated enough money in your retirement savings account (RSA) during your working years,
+                you
+                can buy your dream house and enjoy a comfortable retirement with the ones you love.
+            </p>
+            <h1>Can I continue to pay my child's fees at retirement?
+            </h1>
+            <p>With the right plans in place, you can continue to pay child's school fees even at retirement. Talk to us
+                today to make sure your pension plan lives up to the retirement you deserve.
+            </p>
+            <h1>Can I buy my dream car at retirement?
+            </h1>
+            <p>Retirement is an adventure in itself and you can totally enjoy the ride in your brand-new dream car. With
+                Voluntary Contributions made to your RSA today, your tomorrow will be filled with making your dreams come
+                true.
+            </p>
+            <button class="py-2 px-4 text-white bg-pink-800 ml-0 font-sans rounded w-28">Start Now</button>
     </div>
+</div>
 </template>
