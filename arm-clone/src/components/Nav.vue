fc6754c94e405bd2a6996a3c73b283e5631a0c39
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-15 11:10:31 +0100

    Update Nav.vue

--- arm-clone/src/components/Nav.vue
+++ arm-clone/src/components/Nav.vue
@@ -3,7 +3,7 @@ import logo from "../assets/images/arm_logo.jpg"
 </script>
 
 <template>
-    <div class="ml-10 w-11/12 mt-2">
+    <div class="ml-10 w-11/12 mt-2 flex justify-between items-center">
         <img :src="logo" alt="ARM Pension Logo" class="w-28">
         <a href="#">ABOUT US</a>
         <a href="#">RETIREMENT PLANS</a>
