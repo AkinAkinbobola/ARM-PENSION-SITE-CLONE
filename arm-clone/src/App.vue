606fdda57db202d64db70aba3ed6e0eda45ba5bf
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-15 12:14:55 +0100

    Comment out swiper

--- arm-clone/src/App.vue
+++ arm-clone/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from './components/Header.vue';
+import Slider from './components/Slider.vue';
 
 </script>
 
 <template>
   <div class="none">
     <Header />
+    <Slider />
   </div>
 </template>
--- arm-clone/src/components/Slider.vue
+++ arm-clone/src/components/Slider.vue
@@ -1,9 +1,22 @@
 <script setup>
-
+// import { Swiper, SwiperSlide } from 'swiper/vue';
+// import { Pagination, Autoplay } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import { ref } from "vue";
+// const sliderImages = ref([
+//     { img: "../assets/images/slider_images/1.jpg" },
+//     { img: "../assets/images/slider_images/2.jpg" },
+//     { img: "../assets/images/slider_images/3.jpg" }
+// ]);
+// const modules = [Pagination, Autoplay]
 </script>
 
 <template>
-    <div>
-        <p>Slider</p>
-    </div>
+    <!-- <swiper :modules="modules" :slides-per-view="3" :space-between="50" navigation :pagination="{ clickable: true }"
+        :scrollbar="{ draggable: true }" :autoplay="{ delay: 7000, disableOnInteraction: false, pauseOnMouseEnter: true }">
+        <swiper-slide v-for="image in sliderImages" :key="image">
+            <img :src="image.img" alt="" srcset="">
+        </swiper-slide>
+    </swiper> -->
 </template>
