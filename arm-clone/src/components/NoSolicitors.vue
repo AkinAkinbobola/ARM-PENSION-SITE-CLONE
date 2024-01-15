dd5cc87efa6d63abb865dd4898242e9038c32ef7
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-15 14:12:12 +0100

    Create NoSolicitors.vue

---
+++ arm-clone/src/components/NoSolicitors.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <img src="https://armpension.com/_nuxt/img/bbbd5c9.png" alt="" srcset="">
+        <h1>No Soliciting</h1>
+        <p>ARM Pension Managers (PFA) Limited does not solicit nor request for payment as a condition to processing your
+            retirement benefits or providing you with information in relation to your retirement savings account with us.
+            Please disregard any request for cash or gift from any employee or agent or organization as a condition for
+            accessing your benefits
+        </p>
+</div></template>
