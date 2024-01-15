958aa12314097be86e65e5ddc58a489b9fb4a91a
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-15 11:17:56 +0100

    Animation

--- arm-clone/src/components/Nav.vue
+++ arm-clone/src/components/Nav.vue
@@ -4,14 +4,15 @@ import logo from "../assets/images/arm_logo.jpg"
 
 <template>
     <div class="ml-10 w-11/12 mt-2 flex justify-between items-center">
-        <img :src="logo" alt="ARM Pension Logo" class="w-28">
-        <a href="#">ABOUT US</a>
-        <a href="#">RETIREMENT PLANS</a>
-        <a href="#">MULTI FUND STRUCTURE</a>
-        <a href="#">PORTALS</a>
-        <a href="#">MEDIA</a>
-        <a href="#">CONTACT</a>
-        <a href="#">FEEDBACK</a>
-        <a href="#">CAREERS</a>
+      <img :src="logo" alt="ARM Pension Logo" class="w-28">
+      <a href="#" class="border-b-2 border-transparent hover:border-pink-900 transition">ABOUT US</a>
+      <a href="#" class="border-b-2 border-transparent hover:border-pink-900 transition">RETIREMENT PLANS</a>
+      <a href="#" class="border-b-2 border-transparent hover:border-pink-900 transition">MULTI FUND STRUCTURE</a>
+      <a href="#" class="border-b-2 border-transparent hover:border-pink-900 transition">PORTALS</a>
+      <a href="#" class="border-b-2 border-transparent hover:border-pink-900 transition">MEDIA</a>
+      <a href="#" class="border-b-2 border-transparent hover:border-pink-900 transition">CONTACT</a>
+      <a href="#" class="border-b-2 border-transparent hover:border-pink-900 transition">FEEDBACK</a>
+      <a href="#" class="border-b-2 border-transparent hover:border-pink-900 transition">CAREERS</a>
     </div>
-</template>
+  </template>
+  
