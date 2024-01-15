1625b9423b598e96ce09e5a17bf77148d3596dad
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-15 15:50:04 +0100

    Update GridSection.vue

--- arm-clone/src/components/GridSection.vue
+++ arm-clone/src/components/GridSection.vue
@@ -1,12 +1,31 @@
 <template>
-    <div class="bg-cream">
-        <h1>Tomorrow is looking good.</h1>
-        <p>Retirement is supposed to be enjoyable and stress-free, so we created our planning experience to be that way too.
-        </p>
-        <p>This isn't a typical retirement planning experience. It's delightful, research-backed, and focuses on what's most
-            important: you.
-        </p>
-        <button class="py-2 px-8 text-white bg-pink-800 ml-9 font-sans rounded">Make it happen</button>
+    <div class="bg-cream h-[80vh]">
+        <div class="flex pt-10 ml-10">
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 ">
+                <img src="../assets/images/grid/grid1.jpg" class="w-20" alt="Image 1">
+                <img src="../assets/images/grid/grid2.jpg" class="w-20" alt="Image 2">
+                <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                    <img src="../assets/images/grid/grid3.jpg" class="w-20" alt="Image 3">
+                    <img src="../assets/images/grid/grid4.jpg" class="w-20" alt="Image 4">
+                    <img src="../assets/images/grid/grid5.jpg" class="w-20" alt="Image 5">
+                </div>
+            </div>
+
+            <div class="ml-10 flex flex-col w-4/12">
+                <h1 class="text-blueText text-5xl font-serif mb-6">Tomorrow is looking good.</h1>
+                <p class="text-lg">Retirement is supposed to be enjoyable and stress-free, so we created our planning
+                    experience to be that way too.
+                </p>
+                <p class="text-lg mt-6 mb-6">This isn't a typical retirement planning experience. It's delightful,
+                    research-backed,
+                    and focuses on what's most
+                    important: you.
+                </p>
+                <button class="py-2 px-8 text-white bg-pink-800 ml-9 font-sans rounded w-52">Make it happen</button>
+            </div>
+        </div>
+
+
 
     </div>
 </template>
