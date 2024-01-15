2f1344c713e1a7bc20e22fef3e5bebb489dd5cb8
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-15 10:47:21 +0100

    Update Nav.vue

--- arm-clone/src/components/Nav.vue
+++ arm-clone/src/components/Nav.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
-        
+        <a href="#">ABOUT US</a>
+        <a href="#">RETIREMENT PLANS</a>
+        <a href="#">MULTI FUND STRUCTURE</a>
+        <a href="#">PORTALS</a>
+        <a href="#">MEDIA</a>
+        <a href="#">CONTACT</a>
+        <a href="#">FEEDBACK</a>
+        <a href="#">CAREERS</a>
     </div>
 </template>
