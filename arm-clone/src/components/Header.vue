a87e1aa83a83a9820ae18278db159b167acfff33
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-15 10:36:54 +0100

    Update Header.vue

--- arm-clone/src/components/Header.vue
+++ arm-clone/src/components/Header.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="flex justify-end w-11/12">
-        <a href="#">Chat with us on Whatsapp</a>
-        <span><a href="#">01-2715000</a> or <a href="#">0700CALLARM</a></span>
-        <button class="text-pink-700">Get Started</button>
+    <div class="flex justify-end w-11/12 items-center">
+        <a href="#" class="text-pink-800 mr-4 text-sm">Chat with us on Whatsapp</a>
+        <span class="text-pink-800 text-sm">
+            <a href="#">01-2715000</a> or <a href="#">0700CALLARM</a>
+        </span>
+        <button class="py-2 px-8 text-white bg-pink-800 ml-9">Get Started</button>
     </div>
 </template>
+  
