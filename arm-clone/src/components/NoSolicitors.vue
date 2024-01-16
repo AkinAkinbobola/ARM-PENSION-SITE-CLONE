8d9e439e415379b1eb7723f00e82ec255df9f9fa
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-16 15:20:21 +0100

    Update NoSolicitors.vue

--- arm-clone/src/components/NoSolicitors.vue
+++ arm-clone/src/components/NoSolicitors.vue
@@ -1,19 +1,20 @@
 <template>
-    <div class="bg-yellow flex items-center justify-center py-20 md:flex-col">
-        <img src="../assets//images/soliciting.png" class="w-28" />
-    
-        <div class="ml-8 flex flex-col items-baseline">
-            <div class="flex items-center">
-                <h1 class="text-4xl text-blueText font-serif">No Soliciting</h1>
-                <font-awesome-icon :icon="['fas', 'circle-xmark']" class="ml-2" />
-            </div>
-    
-            <p class="w-96 text-justify mt-4 ml-16">
-                ARM Pension Managers (PFA) Limited does not solicit nor request for payment as a condition to processing your
-                retirement benefits or providing you with information in relation to your retirement savings account with us.
-                Please disregard any request for cash or gift from any employee or agent or organization as a condition for
-                accessing your benefits.
-            </p>
+    <div class="bg-yellow flex items-center justify-center flex-col md:flex-row py-20">
+      <img src="../assets/images/soliciting.png" class="w-28" />
+  
+      <div class="ml-8 flex flex-col items-center md:items-baseline">
+        <div class="flex items-center">
+          <h1 class="text-4xl text-blueText font-serif">No Soliciting</h1>
+          <font-awesome-icon :icon="['fas', 'circle-xmark']" class="ml-2 hidden md:block" />
         </div>
-    </div>    
-</template>
+  
+        <p class="w-96 text-justify mt-4 md:ml-16">
+          ARM Pension Managers (PFA) Limited does not solicit nor request for payment as a condition to processing your
+          retirement benefits or providing you with information in relation to your retirement savings account with us.
+          Please disregard any request for cash or gift from any employee or agent or organization as a condition for
+          accessing your benefits.
+        </p>
+      </div>
+    </div>
+  </template>
+  
