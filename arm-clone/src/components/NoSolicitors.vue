bdf89ffec2a0be87c4320fe7463913dbf0d0d38f
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-16 10:25:41 +0100

    Update NoSolicitors.vue

--- arm-clone/src/components/NoSolicitors.vue
+++ arm-clone/src/components/NoSolicitors.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="bg-yellow h-[60vh] flex items-center justify-center ">
-        <img src="https://armpension.com/_nuxt/img/bbbd5c9.png" class="w-28">
-        <div class="ml-20 flex flex-col items-baseline">
-            <h1 class="text-4xl text-blueText font-serif ml-10 pt-6 pb-10">No Soliciting</h1>
-            <p class="w-96 text-justify">ARM Pension Managers (PFA) Limited does not solicit nor request for payment as a
-                condition to processing your
-                retirement benefits or providing you with information in relation to your retirement savings account with
-                us.
+    <div class="bg-yellow h-[60vh] flex items-center justify-center">
+        <img src="https://armpension.com/_nuxt/img/bbbd5c9.png" class="w-28" />
+    
+        <div class="ml-8 flex flex-col items-baseline">
+            <div class="flex items-center">
+                <h1 class="text-4xl text-blueText font-serif">No Soliciting</h1>
+                <font-awesome-icon :icon="['fas', 'circle-xmark']" class="ml-2" />
+            </div>
+    
+            <p class="w-96 text-justify mt-4 ml-16">
+                ARM Pension Managers (PFA) Limited does not solicit nor request for payment as a condition to processing your
+                retirement benefits or providing you with information in relation to your retirement savings account with us.
                 Please disregard any request for cash or gift from any employee or agent or organization as a condition for
-                accessing your benefits
+                accessing your benefits.
             </p>
         </div>
-
-    </div>
+    </div>    
 </template>
