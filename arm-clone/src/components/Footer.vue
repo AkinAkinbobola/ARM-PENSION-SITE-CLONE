91d35fd6c95dccf376e178089ba5829149e20349
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-16 13:23:41 +0100

    Update Footer.vue

--- arm-clone/src/components/Footer.vue
+++ arm-clone/src/components/Footer.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="bg-footerBlack text-white flex py-14 px-10">
-        <div class="flex flex-col">
+        <div class="flex flex-col w-2/5 mr-14">
             <h1 class="font-bold">CONTACT US</h1>
-            <p class="text-xs">5, Mekunwen Rd. Off Oyinkan Abayomi Drive PMB 80173, Ikoyi, Lagos, Nigeria</p>
-            <p class="text-xs">+234 1 2715005(Head Office)</p>
-            <p class="text-xs">+234 1 2715000(Call Centre)</p>
-            <p class="text-xs">+234 1 3305000(Call Centre)</p>
-            <p class="text-xs">+234 1 4650000(Call Centre)</p>
-            <p class="text-xs">0700CALLARM (07002255276) Call Centre</p>
-            <a href="#" class="text-footerGrey hover:underline text-xs">Make a complaint</a>
-            <a href="#" class="text-footerGrey hover:underline text-xs">Find a branch</a>
-            <a href="#" class="text-footerGrey hover:underline text-xs">Privacy Policy</a>
-            <a href="#" class="text-footerGrey hover:underline text-xs">Cookie Policy</a>
+            <p class="text-sm font-sans mb-3">5, Mekunwen Rd. Off Oyinkan Abayomi Drive PMB 80173, Ikoyi, Lagos, Nigeria</p>
+            <p class="text-sm font-sans mb-1">+234 1 2715005(Head Office)</p>
+            <p class="text-sm font-sans mb-1">+234 1 2715000(Call Centre)</p>
+            <p class="text-sm font-sans mb-1">+234 1 3305000(Call Centre)</p>
+            <p class="text-sm font-sans mb-1">+234 1 4650000(Call Centre)</p>
+            <p class="text-sm font-sans mb-1">0700CALLARM (07002255276) Call Centre</p>
+            <a href="#" class="text-footerGrey hover:underline text-sm mb-1">Make a complaint</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm mb-1">Find a branch</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm mb-1">Privacy Policy</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm mb-1">Cookie Policy</a>
         </div>
 
-        <div class="flex flex-col">
+        <div class="flex flex-col w-1/4">
             <h1 class="font-bold">USEFUL LINKS</h1>
             <a href="#" class="text-footerGrey hover:underline text-xs">Daily Unit Prices</a>
             <a href="#" class="text-footerGrey hover:underline text-xs">Careers</a>
@@ -25,7 +25,7 @@
             <a href="#" class="text-footerGrey hover:underline text-xs">Whistle Blowing</a>
             <a href="#" class="text-footerGrey hover:underline text-xs">Public Notice on abuse of the Pensions Scheme</a>
         </div>
-        <div class="flex flex-col">
+        <div class="flex flex-col w-1/3">
             <h1 class="font-bold">USEFUL LINKS</h1>
             <a href="#" class="text-footerGrey hover:underline text-xs">Frequently Asked Questions</a>
             <a href="#" class="text-footerGrey hover:underline text-xs">Monthly Digest and Newsletters</a>
@@ -37,7 +37,7 @@
             <img src="../assets/images/download/apple.png">
 
         </div>
-        <div>
+        <div class="flex flex-col w-1/3">
             <h1 class="font-bold">SOCIALS</h1>
             <div class="flex">
                 <div class="w-10 h-10 bg-white rounded-full flex items-center justify-center">
