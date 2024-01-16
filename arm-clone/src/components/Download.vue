93ad09cbfe5a3767820e817000f0d2ee514d9dff
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-16 11:43:00 +0100

    Add images and lay out the content

--- arm-clone/src/components/Download.vue
+++ arm-clone/src/components/Download.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        
+    <div class="bg-cream flex">
+        <div class="flex flex-col">
+            <h1>Manage your account from anywhere</h1>
+            <p>With the ARM Engage app, you take your retirement account with you everywhere. Download the app today from
+                the Google play store or iOS App store to enjoy easy access.
+            </p>
+            <div class="flex">
+                <img src="../assets/images/download/google.png" class="w-28">
+                <img src="../assets/images/download/apple.png" class="w-28">
+            </div>
+        </div>
+        <img src="../assets/images/download/main.png" class="w-2/4">
     </div>
 </template>
