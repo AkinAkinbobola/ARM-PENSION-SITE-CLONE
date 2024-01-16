f221365d5a70b92fd8898101115038a74b0831ab
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-16 11:53:00 +0100

    Update Download.vue

--- arm-clone/src/components/Download.vue
+++ arm-clone/src/components/Download.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="bg-cream flex">
+    <div class="bg-cream flex pt-20 px-20 items-center">
         <div class="flex flex-col">
-            <h1>Manage your account from anywhere</h1>
-            <p>With the ARM Engage app, you take your retirement account with you everywhere. Download the app today from
+            <h1 class="text-blueText text-3xl font-serif font-semibold mb-4">Manage your account from anywhere</h1>
+            <p class="text-base mb-10">With the ARM Engage app, you take your retirement account with you everywhere. Download the app today from
                 the Google play store or iOS App store to enjoy easy access.
             </p>
             <div class="flex">
-                <img src="../assets/images/download/google.png" class="w-28">
+                <img src="../assets/images/download/google.png" class="w-28 mr-6">
                 <img src="../assets/images/download/apple.png" class="w-28">
             </div>
         </div>
-        <img src="../assets/images/download/main.png" class="w-2/4">
+        <img src="../assets/images/download/main.png" class="w-2/4 ml-5">
     </div>
 </template>
