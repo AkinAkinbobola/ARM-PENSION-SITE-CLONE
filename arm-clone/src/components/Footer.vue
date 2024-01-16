661af02ef7c1a95604b3974dc31923082f342c36
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-16 13:33:18 +0100

    Update Footer.vue

--- arm-clone/src/components/Footer.vue
+++ arm-clone/src/components/Footer.vue
@@ -2,39 +2,39 @@
     <div class="bg-footerBlack text-white flex py-14 px-10">
         <div class="flex flex-col w-2/5 mr-14">
             <h1 class="font-bold mb-4">CONTACT US</h1>
-            <p class="text-sm font-sans mb-3">5, Mekunwen Rd. Off Oyinkan Abayomi Drive PMB 80173, Ikoyi, Lagos, Nigeria</p>
-            <p class="text-sm font-sans mb-1">+234 1 2715005(Head Office)</p>
-            <p class="text-sm font-sans mb-1">+234 1 2715000(Call Centre)</p>
-            <p class="text-sm font-sans mb-1">+234 1 3305000(Call Centre)</p>
-            <p class="text-sm font-sans mb-1">+234 1 4650000(Call Centre)</p>
-            <p class="text-sm font-sans mb-1">0700CALLARM (07002255276) Call Centre</p>
-            <a href="#" class="text-footerGrey hover:underline text-sm mb-1">Make a complaint</a>
-            <a href="#" class="text-footerGrey hover:underline text-sm mb-1">Find a branch</a>
-            <a href="#" class="text-footerGrey hover:underline text-sm mb-1">Privacy Policy</a>
-            <a href="#" class="text-footerGrey hover:underline text-sm mb-1">Cookie Policy</a>
+            <p class="text-sm font-sans mb-2">5, Mekunwen Rd. Off Oyinkan Abayomi Drive PMB 80173, Ikoyi, Lagos, Nigeria</p>
+            <p class="text-sm font-sans mb-2">+234 1 2715005(Head Office)</p>
+            <p class="text-sm font-sans mb-2">+234 1 2715000(Call Centre)</p>
+            <p class="text-sm font-sans mb-2">+234 1 3305000(Call Centre)</p>
+            <p class="text-sm font-sans mb-2">+234 1 4650000(Call Centre)</p>
+            <p class="text-sm font-sans mb-2">0700CALLARM (07002255276) Call Centre</p>
+            <a href="#" class="text-footerGrey hover:underline text-sm mb-2">Make a complaint</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm mb-2">Find a branch</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm mb-2">Privacy Policy</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm mb-2">Cookie Policy</a>
         </div>
 
         <div class="flex flex-col w-1/4">
             <h1 class="font-bold mb-4">USEFUL LINKS</h1>
-            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-1">Daily Unit Prices</a>
-            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-1">Careers</a>
-            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-1">Delete Account</a>
-            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-1">ARM Group</a>
-            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-1">National Pension Commission</a>
-            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-1">Pension Fund Operators Association of Nigeria</a>
-            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-1">Whistle Blowing</a>
-            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-1">Public Notice on abuse of the Pensions Scheme</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Daily Unit Prices</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Careers</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Delete Account</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">ARM Group</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">National Pension Commission</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Pension Fund Operators Association of Nigeria</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Whistle Blowing</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Public Notice on abuse of the Pensions Scheme</a>
         </div>
         <div class="flex flex-col w-1/3">
             <h1 class="font-bold mb-4">MEDIA CENTER</h1>
-            <a href="#" class="text-footerGrey hover:underline text-xs">Frequently Asked Questions</a>
-            <a href="#" class="text-footerGrey hover:underline text-xs">Monthly Digest and Newsletters</a>
-            <a href="#" class="text-footerGrey hover:underline text-xs">Pension Reform Act 2014</a>
-            <a href="#" class="text-footerGrey hover:underline text-xs">Regulations and Financial Reports</a>
-            <a href="#" class="text-footerGrey hover:underline text-xs">Notice on Pension Reform Act 2014</a>
-            <a href="#" class="text-footerGrey hover:underline text-xs">Download the Engage App</a>
-            <img src="../assets/images/download/google.png">
-            <img src="../assets/images/download/apple.png">
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Frequently Asked Questions</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Monthly Digest and Newsletters</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Pension Reform Act 2014</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Regulations and Financial Reports</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Notice on Pension Reform Act 2014</a>
+            <a href="#" class="text-footerGrey hover:underline text-sm font-sans mb-2">Download the Engage App</a>
+            <img src="../assets/images/download/google.png" class="w-5/6 mb-2">
+            <img src="../assets/images/download/apple.png" class="w-5/6">
 
         </div>
         <div class="flex flex-col w-1/3">
