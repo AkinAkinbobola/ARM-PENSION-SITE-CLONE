fe8e30c937ea507d687ebda59315aa310c11d366
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-16 15:36:02 +0100

    Update GridSection.vue

--- arm-clone/src/components/GridSection.vue
+++ arm-clone/src/components/GridSection.vue
@@ -1,29 +1,25 @@
 <template>
-    <div class="bg-cream pb-20">
-        <div class="flex pt-10 ml-10">
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 ">
-                <img src="../assets/images/grid/grid1.jpg" class="w-20" alt="Image 1">
-                <img src="../assets/images/grid/grid2.jpg" class="w-20" alt="Image 2">
-                <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                    <img src="../assets/images/grid/grid3.jpg" class="w-20" alt="Image 3">
-                    <img src="../assets/images/grid/grid4.jpg" class="w-20" alt="Image 4">
-                    <img src="../assets/images/grid/grid5.jpg" class="w-20" alt="Image 5">
-                </div>
-            </div>
-
-            <div class="ml-10 flex flex-col w-5/12 mr-10">
-                <h1 class="text-blueText text-4xl font-serif mb-6 font-bold">Tomorrow is looking good.</h1>
-                <p class="text-lg">Retirement is supposed to be enjoyable and stress-free, so we created our planning
-                  experience to be that way too.
-                </p>
-                <p class="text-lg mt-6 mb-6">This isn't a typical retirement planning experience. It's delightful, research-backed,
-                  and focuses on what's most important: you.
-                </p>
-                <button class="py-2 px-8 text-white bg-pink-800 ml-0 font-sans rounded w-52">Make it happen</button>
-              </div>              
+  <div class="bg-cream pb-20 flex flex-col md:flex-row">
+      <div class="flex">
+        <img src="../assets/images/grid/grid1.jpg" class="w-20" alt="Image 1">
+        <img src="../assets/images/grid/grid2.jpg" class="w-20" alt="Image 2">
+        <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+          <img src="../assets/images/grid/grid3.jpg" class="w-20" alt="Image 3">
+          <img src="../assets/images/grid/grid4.jpg" class="w-20" alt="Image 4">
+          <img src="../assets/images/grid/grid5.jpg" class="w-20" alt="Image 5">
         </div>
+      </div>
 
-
-
-    </div>
+      <div class="ml-10 flex flex-col w-full md:w-5/12 mr-10">
+        <h1 class="text-blueText text-4xl font-serif mb-6 font-bold">Tomorrow is looking good.</h1>
+        <p class="text-lg">Retirement is supposed to be enjoyable and stress-free, so we created our planning
+          experience to be that way too.
+        </p>
+        <p class="text-lg mt-6 mb-6">This isn't a typical retirement planning experience. It's delightful,
+          research-backed,
+          and focuses on what's most important: you.
+        </p>
+        <button class="py-2 px-8 text-white bg-pink-800 ml-0 font-sans rounded w-52">Make it happen</button>
+      </div>
+  </div>
 </template>
