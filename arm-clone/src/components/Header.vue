47e7ff00b3273610257c8f0141bdaf9557719492
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-16 10:03:33 +0100

    Update Header.vue

--- arm-clone/src/components/Header.vue
+++ arm-clone/src/components/Header.vue
@@ -5,13 +5,16 @@ import Nav from './Nav.vue';
 <template>
     <div class="sticky top-0 z-10 bg-white">
         <div class="flex justify-end w-11/12 items-center border-b border-gray-300 ml-10">
-            <a href="#" class="text-primary mr-4 text-xs font-bold font-sans">Chat with us on Whatsapp</a>
+            <a href="#" class="text-primary mr-4 text-xs font-bold font-sans hover:underline hover:text-blue-400">Chat with
+                us on Whatsapp</a>
             <span class="text-primary text-xs font-bold font-sans">
-                <a href="#">01-2715000</a> or <a href="#">0700CALLARM</a>
+                <a href="#" class="hover:underline hover:text-blue-400">01-2715000</a>
+                <span> or </span>
+                <a href="#" class="hover:underline hover:text-blue-400">0700CALLARM</a>
             </span>
             <button class="py-2 px-4 text-white bg-primary ml-9 font-sans">
                 Get Started
-                <font-awesome-icon :icon="['fas', 'arrow-right']" class="pl-1"/>            
+                <font-awesome-icon :icon="['fas', 'arrow-right']" class="pl-1" />
             </button>
         </div>
         <Nav />
