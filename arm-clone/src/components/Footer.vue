8920f8db5a19e515b51687a0251d189770e822e8
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-16 12:11:12 +0100

    Footer layout and content

--- arm-clone/src/components/Footer.vue
+++ arm-clone/src/components/Footer.vue
@@ -1,5 +1,52 @@
 <template>
-    <div>
-        
+    <div class="bg-black text-white flex">
+        <div class="flex flex-col">
+            <h1>CONTACT US</h1>
+            <p>5, Mekunwen Rd. Off Oyinkan Abayomi Drive PMB 80173, Ikoyi, Lagos, Nigeria</p>
+            <p>+234 1 2715005(Head Office)</p>
+            <p>+234 1 2715000(Call Centre)</p>
+            <p>+234 1 3305000(Call Centre)</p>
+            <p>+234 1 4650000(Call Centre)</p>
+            <p>0700CALLARM (07002255276) Call Centre</p>
+            <a href="#">Make a complaint</a>
+            <a href="#">Find a branch</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Cookie Policy</a>
+        </div>
+
+        <div class="flex flex-col">
+            <h1>USEFUL LINKS</h1>
+            <a href="#">Daily Unit Prices</a>
+            <a href="#">Careers</a>
+            <a href="#">Delete Account</a>
+            <a href="#">ARM Group</a>
+            <a href="#">National Pension Commission</a>
+            <a href="#">Pension Fund Operators Association of Nigeria</a>
+            <a href="#">Whistle Blowing</a>
+            <a href="#">Public Notice on abuse of the Pensions Scheme</a>
+        </div>
+        <div class="flex flex-col">
+            <h1>USEFUL LINKS</h1>
+            <a href="#">Frequently Asked Questions</a>
+            <a href="#">Monthly Digest and Newsletters</a>
+            <a href="#">Pension Reform Act 2014</a>
+            <a href="#">Regulations and Financial Reports</a>
+            <a href="#">Notice on Pension Reform Act 2014</a>
+            <a href="#">Download the Engage App</a>
+        </div>
+        <div>
+            <h1>SOCIALS</h1>
+            <font-awesome-icon :icon="['fab', 'linkedin-in']" />
+            <font-awesome-icon :icon="['fab', 'twitter']" />
+            <font-awesome-icon :icon="['fab', 'instagram']" />
+            <font-awesome-icon :icon="['fab', 'facebook-f']" />
+            <div class="flex w-28">
+                <img src="../assets/images/footer/footer1.jpg">
+                <img src="../assets/images/footer/footer2.jpg">
+            </div>
+
+            <p>We have taken extra steps to ensure the safety of your information by acquiring the ISO 27001:2013
+                certification for information security.</p>
+        </div>
     </div>
 </template>
