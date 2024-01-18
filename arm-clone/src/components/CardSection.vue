42f7c596dbf822d90af94b56e2d0632fc48d7f3b
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-18 08:10:59 +0100

    Update CardSection.vue

--- arm-clone/src/components/CardSection.vue
+++ arm-clone/src/components/CardSection.vue
@@ -10,9 +10,10 @@ import image from '../assets/images/info.jpg';
 
         <div class="flex flex-wrap mt-4 items-center justify-center md:flex-nowrap mx-4 md:mx-16">
 
-            <div class="bg-cover bg-center w-4/12 h-80 mr-4 mb-4 md:h-96 md:w-2/4 flex flex-col justify-end"
+            <div class="bg-cover bg-center w-4/12 h-80 mr-4 mb-4 md:h-96 md:w-2/4 flex flex-col justify-end relative"
                 :style="{ backgroundImage: `url(${image})` }">
-                <div class="text-white ml-3 mb-8">
+                <div class="bg-black bg-opacity-50 absolute inset-0"></div>
+                <div class="text-white ml-3 mb-8 absolute">
                     <h2 class="text-md font-bold mb-2">Benefit Payment</h2>
                     <div class="flex items-center">
                         <p class="text-md font-semibold">Learn More</p>
@@ -21,68 +22,62 @@ import image from '../assets/images/info.jpg';
                 </div>
             </div>
 
-            <div class="bg-cover bg-center w-4/12 h-80 mb-4 md:h-96 md:w-2/4 md:mr-4 flex flex-col justify-end"
+            <div class="bg-cover bg-center w-4/12 h-80 mb-4 md:h-96 md:w-2/4 md:mr-4 flex flex-col justify-end relative"
                 :style="{ backgroundImage: `url(${image})` }">
-                <div class=" bg-black opacity-50"></div>
-                <div class=" flex flex-col justify-end items-start text-white">
-                    <div class="mb-8 ml-4">
-                        <h2 class="text-sm font-bold mb-4">Voluntary Contributions</h2>
-                        <div class="flex items-center">
-                            <p class="text-sm font-semibold">Learn More</p>
-                            <font-awesome-icon :icon="['fas', 'arrow-right']" class="pl-2" />
-                        </div>
+                <div class="bg-black bg-opacity-50 absolute inset-0"></div>
+                <div class="text-white ml-3 mb-8 absolute">
+                    <h2 class="text-sm font-bold mb-4">Voluntary Contributions</h2>
+                    <div class="flex items-center">
+                        <p class="text-sm font-semibold">Learn More</p>
+                        <font-awesome-icon :icon="['fas', 'arrow-right']" class="pl-2" />
                     </div>
                 </div>
             </div>
 
-            <div class="bg-cover bg-center  w-4/12 h-80 mr-4 mb-4 md:h-96 md:w-2/4 flex flex-col justify-end"
+            <div class="bg-cover bg-center  w-4/12 h-80 mr-4 mb-4 md:h-96 md:w-2/4 flex flex-col justify-end relative"
                 :style="{ backgroundImage: `url(${image})` }">
-                <div class=" bg-black opacity-50"></div>
-                <div class=" flex flex-col justify-end items-start text-white">
-                    <div class="mb-8 ml-4">
-                        <h2 class="text-sm font-bold mb-4">Account Maintenance</h2>
-                        <div class="flex items-center">
-                            <p class="text-sm font-semibold">Learn More</p>
-                            <font-awesome-icon :icon="['fas', 'arrow-right']" class="pl-2" />
-                        </div>
+                <div class="bg-black bg-opacity-50 absolute inset-0"></div>
+                <div class="text-white ml-3 mb-8 absolute">
+                    <h2 class="text-sm font-bold mb-4">Account Maintenance</h2>
+                    <div class="flex items-center">
+                        <p class="text-sm font-semibold">Learn More</p>
+                        <font-awesome-icon :icon="['fas', 'arrow-right']" class="pl-2" />
                     </div>
                 </div>
             </div>
 
-            <div class="bg-cover bg-center  w-4/12 h-80 mb-4 md:h-96 md:w-2/4 flex flex-col justify-end"
+            <div class="bg-cover bg-center  w-4/12 h-80 mb-4 md:h-96 md:w-2/4 flex flex-col justify-end relative"
                 :style="{ backgroundImage: `url(${image})` }">
-                <div class=" bg-black opacity-50"></div>
-                <div class=" flex flex-col justify-end items-start text-white">
-                    <div class="mb-8 ml-4">
-                        <h2 class="text-sm font-bold mb-1">Price History</h2>
-                        <p class="text-xs mt-1 mb-1">
-                            January 10, 2024
-                        </p>
-                        <p class="text-xs mt-1 mb-1">
-                            RSA Fund I: <span class="font-bold">1.8732</span>
-                        </p>
-                        <p class="text-xs mt-1 mb-1">
-                            RSA Fund II: <span class="font-bold">7.1425</span>
-                        </p>
-                        <p class="text-xs mt-1 mb-1">
-                            RSA Fund III: <span class="font-bold">1.8084</span>
-                        </p>
-                        <p class="text-xs mt-1 mb-1">
-                            RSA Fund IV: <span class="font-bold">5.7166</span>
-                        </p>
-                        <p class="text-xs mt-1 mb-1">
-                            RSA Fund V: <span class="font-bold">1.3933</span>
-                        </p>
-                        <p class="text-xs mt-1 mb-1">
-                            RSA Fund VI Active: <span class="font-bold">1.2936</span>
-                        </p>
-                        <p class="text-xs mt-1 mb-1">
-                            RSA Fund VI Retiree: <span class="font-bold">1.2053</span>
-                        </p>
-                        <div class="flex items-center">
-                            <p class="text-sm font-semibold">Learn More</p>
-                            <font-awesome-icon :icon="['fas', 'arrow-right']" class="pl-2" />
-                        </div>
+                <div class="bg-black bg-opacity-50 absolute inset-0"></div>
+                <div class="text-white ml-3 mb-8 absolute">
+                    <h2 class="text-sm font-bold mb-1">Price History</h2>
+                    <p class="text-xs mt-1 mb-1">
+                        January 10, 2024
+                    </p>
+                    <p class="text-xs mt-1 mb-1">
+                        RSA Fund I: <span class="font-bold">1.8732</span>
+                    </p>
+                    <p class="text-xs mt-1 mb-1">
+                        RSA Fund II: <span class="font-bold">7.1425</span>
+                    </p>
+                    <p class="text-xs mt-1 mb-1">
+                        RSA Fund III: <span class="font-bold">1.8084</span>
+                    </p>
+                    <p class="text-xs mt-1 mb-1">
+                        RSA Fund IV: <span class="font-bold">5.7166</span>
+                    </p>
+                    <p class="text-xs mt-1 mb-1">
+                        RSA Fund V: <span class="font-bold">1.3933</span>
+                    </p>
+                    <p class="text-xs mt-1 mb-1">
+                        RSA Fund VI Active: <span class="font-bold">1.2936</span>
+                    </p>
+                    <p class="text-xs mt-1 mb-1">
+                        RSA Fund VI Retiree: <span class="font-bold">1.2053</span>
+                    </p>
+                    <div class="flex items-center">
+                        <p class="text-sm font-semibold">Learn More</p>
+                        <font-awesome-icon :icon="['fas', 'arrow-right']" class="pl-2" />
                     </div>
                 </div>
             </div>
