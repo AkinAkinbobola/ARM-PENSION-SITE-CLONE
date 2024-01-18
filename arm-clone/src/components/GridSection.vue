c4da5e6c6080150dc873a4f1ef0cefa2e6eaccb4
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-18 09:21:24 +0100

    Update GridSection.vue

--- arm-clone/src/components/GridSection.vue
+++ arm-clone/src/components/GridSection.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="bg-cream pb-12 flex flex-col md:flex-row md:py-14 md:px-20 p-6">
+  <div class="bg-cream pb-12 flex flex-col md:flex-row md:py-14 md:px-20 p-6 justify-center">
     <div class="grid">
-      <img src="../assets/images/grid/grid1.jpg" class="w-20" alt="Image 1">
       <img src="../assets/images/grid/grid2.jpg" class="w-20" alt="Image 2">
       <img src="../assets/images/grid/grid3.jpg" class="w-20" alt="Image 3">
-      <img src="../assets/images/grid/grid4.jpg" class="w-20" alt="Image 4">
       <img src="../assets/images/grid/grid5.jpg" class="w-20" alt="Image 5">
+      <img src="../assets/images/grid/grid4.jpg" class="w-20" alt="Image 4">
+      <img src="../assets/images/grid/grid1.jpg" class="w-20" alt="Image 1">
+
     </div>
 
     <div class="md:ml-10 flex flex-col md:w-6/12">
