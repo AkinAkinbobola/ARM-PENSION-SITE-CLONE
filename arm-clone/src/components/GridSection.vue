d27acf48c58cf5768f1cd1fd17ac150b30126038
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-18 09:40:15 +0100

    Update GridSection.vue

--- arm-clone/src/components/GridSection.vue
+++ arm-clone/src/components/GridSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-cream pb-12 flex flex-col md:flex-row md:py-14 md:px-20 p-6 justify-center">
-    <div class="grid grid-cols-3 grid-rows-2 gap-4">
+    <div class="grid grid-cols-3 grid-rows-2 gap-4 mb-6">
       <img src="../assets/images/grid/grid2.jpg" class="w-56 rounded shadow-md col-span-2 object-cover" alt="Image 2">
       <img src="../assets/images/grid/grid3.jpg" class="w-48 rounded shadow-md object-cover" alt="Image 3">
       <img src="../assets/images/grid/grid5.jpg" class="w-48 rounded shadow-md object-cover" alt="Image 5">
@@ -12,10 +12,10 @@
       <h1 class="text-blueText text-4xl font-serif mb-6 font-bold">
         Tomorrow is looking good.
       </h1>
-      <p class="text-lg text-justify">Retirement is supposed to be enjoyable and stress-free, so we created our planning
+      <p class="text-md text-justify">Retirement is supposed to be enjoyable and stress-free, so we created our planning
         experience to be that way too.
       </p>
-      <p class="text-lg mt-6 mb-6 text-justify">This isn't a typical retirement planning experience. It's delightful,
+      <p class="text-md mt-6 mb-6 text-justify">This isn't a typical retirement planning experience. It's delightful,
         research-backed,
         and focuses on what's most important: you.
       </p>
