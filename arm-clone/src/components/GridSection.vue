008a67842a66b404fac15cc32606a1b5f761079c
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-18 09:45:32 +0100

    Update GridSection.vue

--- arm-clone/src/components/GridSection.vue
+++ arm-clone/src/components/GridSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-cream pb-12 flex flex-col md:flex-row md:py-14 md:px-20 p-6 justify-center">
     <div class="grid grid-cols-3 grid-rows-2 gap-4 mb-6">
-      <img src="../assets/images/grid/grid2.jpg" class="w-56 rounded shadow-md col-span-2 object-cover">
+      <img src="../assets/images/grid/grid2.jpg" class="h-56 rounded shadow-md col-span-2 object-cover">
       <img src="../assets/images/grid/grid3.jpg" class="w-48 rounded shadow-md object-cover">
       <img src="../assets/images/grid/grid5.jpg" class="w-48 h-36 rounded shadow-md object-cover">
       <img src="../assets/images/grid/grid4.jpg" class="w-56 h-60 object-cover rounded shadow-md">
