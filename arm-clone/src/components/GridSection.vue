1032f550ea6dd47a4b1a767983da9cae869255a9
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-18 08:53:23 +0100

    Remove grid and format code

--- arm-clone/src/components/GridSection.vue
+++ arm-clone/src/components/GridSection.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="bg-cream pb-12 flex flex-col md:flex-row md:py-14 md:px-20 p-6">
-      <div class="flex">
-        <img src="../assets/images/grid/grid1.jpg" class="w-20" alt="Image 1">
-        <img src="../assets/images/grid/grid2.jpg" class="w-20" alt="Image 2">
-        <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
-          <img src="../assets/images/grid/grid3.jpg" class="w-20" alt="Image 3">
-          <img src="../assets/images/grid/grid4.jpg" class="w-20" alt="Image 4">
-          <img src="../assets/images/grid/grid5.jpg" class="w-20" alt="Image 5">
-        </div>
-      </div>
+    <div class="grid">
+      <img src="../assets/images/grid/grid1.jpg" class="w-20" alt="Image 1">
+      <img src="../assets/images/grid/grid2.jpg" class="w-20" alt="Image 2">
+      <img src="../assets/images/grid/grid3.jpg" class="w-20" alt="Image 3">
+      <img src="../assets/images/grid/grid4.jpg" class="w-20" alt="Image 4">
+      <img src="../assets/images/grid/grid5.jpg" class="w-20" alt="Image 5">
+    </div>
 
-      <div class="md:ml-10 flex flex-col md:w-6/12">
-        <h1 class="text-blueText text-4xl font-serif mb-6 font-bold">
-          Tomorrow is looking good.
-        </h1>
-        <p class="text-lg text-justify">Retirement is supposed to be enjoyable and stress-free, so we created our planning
-          experience to be that way too.
-        </p>
-        <p class="text-lg mt-6 mb-6 text-justify">This isn't a typical retirement planning experience. It's delightful,
-          research-backed,
-          and focuses on what's most important: you.
-        </p>
-        <button class="py-2 px-8 text-white bg-pink-800 ml-0 font-sans rounded w-52">Make it happen</button>
-      </div>
+    <div class="md:ml-10 flex flex-col md:w-6/12">
+      <h1 class="text-blueText text-4xl font-serif mb-6 font-bold">
+        Tomorrow is looking good.
+      </h1>
+      <p class="text-lg text-justify">Retirement is supposed to be enjoyable and stress-free, so we created our planning
+        experience to be that way too.
+      </p>
+      <p class="text-lg mt-6 mb-6 text-justify">This isn't a typical retirement planning experience. It's delightful,
+        research-backed,
+        and focuses on what's most important: you.
+      </p>
+      <button class="py-2 px-8 text-white bg-pink-800 ml-0 font-sans rounded w-52">Make it happen</button>
+    </div>
   </div>
 </template>
--- arm-clone/src/components/NoSolicitors.vue
+++ arm-clone/src/components/NoSolicitors.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="bg-yellow flex items-center justify-center flex-col md:flex-row py-20">
-      <img src="../assets/images/soliciting.png" class="w-28" />
-  
-      <div class="ml-8 flex flex-col items-center md:items-baseline">
-        <div class="flex justify-between items-center w-10/12">
-          <h1 class="text-4xl text-blueText font-serif my-4 md:my-0">No Soliciting</h1>
-          <font-awesome-icon :icon="['fas', 'circle-xmark']" class="mt-3 hidden md:block" />
-        </div>
-  
-        <p class="w-96 text-justify mt-4 md:ml-16">
-          ARM Pension Managers (PFA) Limited does not solicit nor request for payment as a condition to processing your
-          retirement benefits or providing you with information in relation to your retirement savings account with us.
-          Please disregard any request for cash or gift from any employee or agent or organization as a condition for
-          accessing your benefits.
-        </p>
-        <p class="text-lg font-sans text-blueText underline font-semibold mt-10 md:hidden">Close Caveat</p>
+  <div class="bg-yellow flex items-center justify-center flex-col md:flex-row py-20">
+    <img src="../assets/images/soliciting.png" class="w-28" />
+
+    <div class="ml-8 flex flex-col items-center md:items-baseline">
+      <div class="flex justify-between items-center w-10/12">
+        <h1 class="text-4xl text-blueText font-serif my-4 md:my-0">No Soliciting</h1>
+        <font-awesome-icon :icon="['fas', 'circle-xmark']" class="mt-3 hidden md:block" />
       </div>
+
+      <p class="w-96 text-justify mt-4 md:ml-16">
+        ARM Pension Managers (PFA) Limited does not solicit nor request for payment as a condition to processing your
+        retirement benefits or providing you with information in relation to your retirement savings account with us.
+        Please disregard any request for cash or gift from any employee or agent or organization as a condition for
+        accessing your benefits.
+      </p>
+      <p class="text-lg font-sans text-blueText underline font-semibold mt-10 md:hidden">Close Caveat</p>
     </div>
-  </template>
+  </div>
+</template>
   
